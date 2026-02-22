2f1bbc2eb69035fad99190b6d35af442b3a1046e
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-02-22 07:43:33 +0530

    single

--- oops.cpp
+++ oops.cpp
@@ -2,25 +2,85 @@
 
 using namespace std;
 
- class student{
-    private:
-    string name;
-    int age;
-     int height;
+//  class student{
+//     private:
+//     string name;
+//     int age;
+//      int height;
 
+//      public:
+//      int getAge(){
+//         return this->age;
+
+//      }
+
+
+//  };
+//  int main(){
+//     student first;
+//     cout<<" sb sahi cha; raha hy "<<endl;
+//     return 0;
+
+
+
+//  }
+
+// inheritance 
+// class Human{
+//     public :
+//     int height;
+//     int weight;
+
+//     int age;
+
+//      public : 
+//      int getage()
+//     {
+//         return this->age;
+//     }
+//     void  setWeight(int w){
+//          this->weight =w;
+
+//     }
+// };
+//  class Male :public Human{
+//     public:
+//     string color;
+
+
+//  };
+
+// int main(){
+//     Male object1;
+//      cout<<object1.age;
+//      cout<<object1.weight;
+//      cout<<object1.height;
+//      object1.setWeight(234);
+//      cout<<object1.weight;
+
+
+
+
+// }
+
+// single inharitance 
+
+class Animal{
+    public:
+    int age;
+     int weight;
      public:
-     int getAge(){
-        return this->age;
+     void speak(){
+        cout<<"speaking"<<endl;
 
      }
-
+};
+ class Dog : public Animal{
 
  };
- int main(){
-    student first;
-    cout<<" sb sahi cha; raha hy "<<endl;
-    return 0;
-
-
-
- }
+int main(){
+    Dog d;
+    d.speak();
+    cout<<d.age<<endl;
+    
+}
