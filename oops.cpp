f06808fb0e4f4bf98eb34f970b3f7bf5a89aaafa
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-02-22 06:44:14 +0530

    encap

---
+++ oops.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+ class student{
+    private:
+    string name;
+    int age;
+     int height;
+
+     public:
+     int getAge(){
+        return this->age;
+
+     }
+
+
+ };
+ int main(){
+    student first;
+    cout<<" sb sahi cha; raha hy "<<endl;
+    return 0;
+
+
+
+ }
